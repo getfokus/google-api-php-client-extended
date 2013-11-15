d29da79c881e3534c43fa1b689450db179e4bbb3
Athlan <athlan@vgroup.pl>
2013-11-15 23:57:16 +0100

    Webmaster tools API improved. Methods added.

--- src/contrib/Google_WebmasterToolsService.php
+++ src/contrib/Google_WebmasterToolsService.php
@@ -32,8 +32,12 @@ class Google_WebmasterToolsService extends Google_Service {
     $this->serviceName = 'webmasterTools';
     
     $this->client = $client;
-	
-	$this->sites = new Google_WebmasterToolsSitesService($this, $client);
+    
+    $this->sites = new Google_WebmasterToolsSitesService($this, $client);
+    $this->messages = new Google_WebmasterToolsMessagesService($this, $client);
+    $this->sitemaps = new Google_WebmasterToolsSitemapsService($this, $client);
+    $this->crawl = new Google_WebmasterToolsCrawlService($this, $client);
+    $this->keywords = new Google_WebmasterToolsKeywordsService($this, $client);
   }
 }
 
@@ -41,19 +45,97 @@ class Google_WebmasterToolsSitesService extends Google_ServiceFeedResource {
   
   public function listAll() {
     $methodPath = 'sites';
-	
+    
     $response = $this->sendFeedRequest($methodPath);
-	$data = $this->parseResponse($response);
-	
-	return $data['entry'];
+    $data = $this->parseResponse($response);
+    
+    return $data['entry'];
   }
   
   public function getByUrl($siteUrl) {
     $methodPath = 'sites/' . urlencode($siteUrl);
-	//var_dump($methodPath);exit;
+    
     $response = $this->sendFeedRequest($methodPath);
-	$data = $this->parseResponse($response);
+    $data = $this->parseResponse($response);
+    
+    return $data;
+  }
+}
+
+class Google_WebmasterToolsKeywordsService extends Google_ServiceFeedResource {
+  
+  public function listAll($siteUrl) {
+    $methodPath = urlencode($siteUrl) . '/keywords/';
+    
+    $response = $this->sendFeedRequest($methodPath);
+    $data = $this->parseResponse($response);
+    
+	if(isset($data['wt:keyword']))
+	  return $data['wt:keyword'];
 	
-	return $data;
+    return array();
+  }
+}
+
+class Google_WebmasterToolsMessagesService extends Google_ServiceFeedResource {
+  
+  public function listAll() {
+    $methodPath = 'messages';
+    
+    $response = $this->sendFeedRequest($methodPath);
+    $data = $this->parseResponse($response);
+    
+	if(!isset($data['entry']))
+	  return array();
+	
+	if(isset($data['entry']['id'])) // only one item
+	  return array($data);
+	
+    return $data['entry'];
+  }
+  
+  public function get($messageId) {
+    $methodPath = 'messages/' . urlencode($messageId);
+    
+    $response = $this->sendFeedRequest($methodPath);
+    $data = $this->parseResponse($response);
+    
+    return $data;
+  }
+}
+
+class Google_WebmasterToolsSitemapsService extends Google_ServiceFeedResource {
+  
+  public function listAll($siteUrl) {
+    $methodPath = urlencode($siteUrl) . '/sitemaps/';
+    
+    $response = $this->sendFeedRequest($methodPath);
+    $data = $this->parseResponse($response);
+	
+	if(!isset($data['entry']))
+	  return array();
+	
+	if(isset($data['entry']['id'])) // only one item
+	  return array($data);
+	
+    return $data['entry'];
+  }
+}
+
+class Google_WebmasterToolsCrawlService extends Google_ServiceFeedResource {
+  
+  public function listAll($siteUrl) {
+    $methodPath = urlencode($siteUrl) . '/crawlissues/';
+    
+    $response = $this->sendFeedRequest($methodPath);
+    $data = $this->parseResponse($response);
+	
+	if(!isset($data['entry']))
+	  return array();
+	
+	if(isset($data['entry']['id'])) // only one item
+	  return array($data);
+	
+    return $data['entry'];
   }
 }
