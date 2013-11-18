866a81acaf0204363e6a891ebe4e2edf0c5df4ae
Jan Kondratowicz <jan@kondratowicz.pl>
2013-11-18 12:42:13 +0100

    fix whitespace

--- src/service/Google_ServiceFeedResource.php
+++ src/service/Google_ServiceFeedResource.php
@@ -1,35 +1,35 @@
 <?php
 
 class Google_ServiceFeedResource {
-  
-  private $service;
-  private $client;
-  
-  public function __construct(Google_Service $service, Google_Client $client) {
-    $this->service = $service;
-    $this->client = $client;
-  }
-  
-  public function sendFeedRequest($methodPath, array $params = array()) {
-    $url = Google_REST::createRequestUri($this->service->servicePath, $methodPath, $params);
-    
-    $httpRequest = new Google_HttpRequest($url, 'GET', null, null);
-    
-    $response = $this->client->getIo()->authenticatedRequest($httpRequest);
-	
-	return $response;
-  }
-  
-  public function parseResponse($response) {
-	try {
-	  $body = $response->getResponseBody();
-	  
-	  $array = Google_XmlParserUtil::XmlToArray($body);
-	  
-	  return $array;
+
+	private $service;
+	private $client;
+
+	public function __construct(Google_Service $service, Google_Client $client) {
+		$this->service = $service;
+		$this->client = $client;
 	}
-	catch(Exception $e) {
-	  return array();
+
+	public function sendFeedRequest($methodPath, array $params = array()) {
+		$url = Google_REST::createRequestUri($this->service->servicePath, $methodPath, $params);
+
+		$httpRequest = new Google_HttpRequest($url, 'GET', null, null);
+
+		$response = $this->client->getIo()->authenticatedRequest($httpRequest);
+
+		return $response;
 	}
-  }
+
+	public function parseResponse($response) {
+		try {
+			$body = $response->getResponseBody();
+
+			$array = Google_XmlParserUtil::XmlToArray($body);
+
+			return $array;
+		} catch (Exception $e) {
+			return array();
+		}
+	}
+
 }
